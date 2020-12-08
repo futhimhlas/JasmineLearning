01427cf432fa820de72c322f3a2cbbb96d220210
futhimhlas <futhimhl2@gmail.com>
2020-12-08 12:29:53 +0000

    calculator functionality changed and subtraction added

--- jasmine-testing/scripts/calc.js
+++ jasmine-testing/scripts/calc.js
@@ -1,7 +1,27 @@
-function addition(numOne,numTwo){
-    if(typeof(numOne) == "number" && typeof(numTwo) == "number"){
-        return numOne + numTwo;
-    } else {
-        return "Error!";
+Calculator = function(){
+    this.value = 0;
+}
+
+Calculator.prototype.add = function(number){
+    if (typeof(number) == "number") {
+        this.value += number;
+    } else { 
+        alert("Error!");
     }
 }
+
+Calculator.prototype.sub = function(number){
+    if (typeof(number) == "number") {
+        this.value -= number;
+    } else { 
+        alert("Error!");
+    }
+}
+
+/*Calculator.prototype.mul = function(numOne, numTwo){
+    if (typeof(numOne) == "number" && typeof(numTwo) == "nummber") {
+        return numOne * numTwo;
+    } else { 
+        alert("Error!");
+    }
+} */
--- jasmine-testing/spec/calcSpec.js
+++ jasmine-testing/spec/calcSpec.js
@@ -1,13 +1,36 @@
 describe("Calculator", function(){
+
+    beforeEach(function(){
+        calc = new Calculator;
+    });
+
     describe("Addition test", function(){
         it("should return 42", function(){
-            expect(addition(20,22)).toBe(42);
+            calc.add(20);
+            calc.add(22);
+            expect(calc.value).toBe(42);
         });
-        it("should return 42", function(){
-            expect(addition(7,19)).toBe(26);
+        it("should return 26", function(){
+            calc.add(7);
+            calc.add(19);
+            expect(calc.value).toBe(26);
         });
         it ("should return an error if we don't supply two numbers", function(){
-            expect(addition("Hitchhikers", "Guide")).toBe("Error!");
+            spyOn(window, "alert");
+            calc.add("Hitchhikers");
+            expect(window.alert).toHaveBeenCalledWith("Error!")
         });
     });
-});
+    describe(" Subtraction test", function(){
+        it("should return -29", function(){
+            calc.sub(14);
+            calc.sub(15);
+            expect(calc.value).toBe(-29);
+        });
+    });
+ /*   describe("Multiplication test", function(){
+        it("should return 30", function(){
+            expect(calc.mul(3,10)).toBe(30);
+        });
+    });*/
+}); 
